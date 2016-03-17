e04324efb5ace764a3bb02ecfd94583e2ef2dbfe
micahcourey <micahjcourey@gmail.com>
2016-03-16 21:31:46 -0700

    Create a search bar container

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Component } from 'react';
 
+import SearchBar from '../containers/search_bar';
+
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>Weather Charts</h1>
-        <p>An app that allows a user to search for cities and then displays city forcasts in chart form</p>
+        <SearchBar />
       </div>
     );
   }
