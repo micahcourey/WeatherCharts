16ca15a6d3e9f9a643f2f1a921d9b0763acc0a08
micahcourey <micahjcourey@gmail.com>
2016-03-24 21:22:25 -0700

    Add Axios library and use it to make AJAX requests to the open weather api.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const API_KEY = '19c6169735fb5e3ad987bc8870884fed';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
@@ -5,8 +7,10 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
 
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
   };
 }
