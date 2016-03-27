52e27fc8cd3ba9c014b4aea57d50a08ea5196c08
micahcourey <micahjcourey@gmail.com>
2016-03-26 21:34:37 -0700

    Modify the search bar UI to improve the UX

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -21,13 +21,13 @@ export default class SearchBar extends Component {
     return (
       <form onSubmit={this.onFormSubmit} className="input-group">
         <input
-          placeholder="Search for a citie to get the five day forcast"
+          placeholder="Search for a citie to get the citie's five day forcast"
           type="text"
           className="form-control"
           value={this.state.term}
           onChange={this.onInputChange} />
         <span className="input-group-btn">
-          <button type="submit" className="btn btn-secondary">Search</button>
+          <button type="submit" className="btn btn-secondary">Get Forcast</button>
         </span>
       </form>
     );
