e2f00ae20f65db21d8748cb1c4decd61eb62d416
micahcourey <micahjcourey@gmail.com>
2016-04-02 22:53:56 -0700

    Add pressure and humidity charts, avg temp line and units

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -6,16 +6,17 @@ class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
     const temps = cityData.list.map(weather => weather.main.temp);
-    console.log(temps);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
 
     return (
       <tr key={name}>
         <td>
           {name}
         </td>
-        <td>
-          <Chart data={temps} color="blue" />
-        </td>
+        <td><Chart data={temps} color="orange" /></td>
+        <td><Chart data={pressures} color="blue" /></td>
+        <td><Chart data={humidities} color="green" /></td>
       </tr>
     )
   }
