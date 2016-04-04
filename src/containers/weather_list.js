8fb795be816e65aed98b0cf1646acf240985306e
micahcourey <micahjcourey@gmail.com>
2016-04-04 08:35:58 -0700

    Configure google maps to center the map around the searched city by using the open weather API's lon and lat properties

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
@@ -10,12 +11,12 @@ class WeatherList extends Component {
           (temp) => (temp - 273.15) * 1.8 + 32);
     const pressures = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
+    const lon = cityData.city.coord.lon;
+    const lat = cityData.city.coord.lat;
 
     return (
       <tr key={name}>
-        <td>
-          {name}
-        </td>
+        <td><GoogleMap lon={lon} lat={lat} /></td>
         <td><Chart data={temps} color="orange" units="F" /></td>
         <td><Chart data={pressures} color="blue" units="hPa" /></td>
         <td><Chart data={humidities} color="green" units="%" /></td>
